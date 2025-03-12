efa9eef6e82481b2337be504c5ff3d0aa446da2d
Arthur <arthurzapata32@gmail.com>
2025-03-12 03:03:40 -0500

    Enlista nombres

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array que almacenará los nombres
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value.trim();
+    if (nombre == '') {
+        alert("Por favor, inserte un nombre.");
+        return;
+        
+    }else{
+        if (amigos.includes(nombre)) {
+            alert(`El nombre ${nombre} ya está agregado`);
+        }else{
+            amigos.push(nombre);
+            limpiarCaja()
+            console.log(amigos);
+        }
+    }
+}
+
+function limpiarCaja() {
+    document.getElementById("amigo").value = "";
+}
