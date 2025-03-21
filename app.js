6688737c0152dc122a6fec6a0e05908c3d6f6701
Arthur <arthurzapata32@gmail.com>
2025-03-21 18:01:43 -0500

    Quita amigo sorteado

--- app.js
+++ app.js
@@ -6,21 +6,20 @@ let ulResultado = document.getElementById("resultado");
 function agregarAmigo() {
     inputNombre = document.getElementById("amigo").value.trim();
     // Si el nombre está vacío
-    if (inputNombre == '') {
+    if (inputNombre === '') {
         alert("Por favor, ingrese un nombre.");
         return;
-    // Si el nombre ya está incluido   
-    }else{
-        if (amigos.includes(inputNombre)) {
-            alert(`El nombre \"${inputNombre}\" ya está agregado`);
-        // Si el nombre no está incluído, lo agrega
-        }else{
-            amigos.push(inputNombre);
-            ulListaAmigos.innerHTML += `<li>${inputNombre}</li>`;
-            limpiarCaja();
-            console.log(amigos);
-        }
     }
+    // Si el nombre ya está incluido   
+    if (amigos.includes(inputNombre)) {
+        alert(`El nombre \"${inputNombre}\" ya está agregado`);
+        return
+    }
+    // Agrega amigo
+    amigos.push(inputNombre);
+    ulListaAmigos.innerHTML += `<li>${inputNombre}</li>`;
+    limpiarCaja();
+    console.log(amigos);
 }
 
 function limpiarCaja() {
@@ -29,14 +28,22 @@ function limpiarCaja() {
 }
 
 function sortearAmigo() {
-    // Solo sorteará si hay más 2 o más amigos
-    if (amigos.length >= 2) {
-        let amigoSorteado = amigos[Math.floor(Math.random()*amigos.length)];
-        ulResultado.innerHTML = `El amigo secreto es: <li>${amigoSorteado}</li>`;
-    }else{
+    // No sorteará si hay menos de dos amigos
+    if (amigos.length < 2) {
         ulResultado.innerHTML = `<li style="color: red;">
-        Necesitas al menos 2 amigos para sortear.</li>`;
+        Se necesitan al menos dos amigos para el sorteo.</li>`;
+        return
     }
+    let amigoSorteado = amigos[Math.floor(Math.random()*amigos.length)];
+    ulResultado.innerHTML = `El amigo secreto es: <li>${amigoSorteado}</li>`;
+    //amigos = amigos.filter(amigo => amigo !== amigoSorteado);
+    amigos.splice(amigos.indexOf(amigoSorteado),1);
+    let listaItems = ulListaAmigos.querySelectorAll('li');
+    listaItems.forEach((item) => {
+        if (item.textContent === amigoSorteado) {
+            ulListaAmigos.removeChild(item);
+        }
+    });
     return
 }
 
